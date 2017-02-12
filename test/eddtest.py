052fa32b3212a6a335cf16ec5bfb410e97632924
rindybell <rindybell@192.168.11.4>
2017-02-12 17:40:36 +0900

    mod test

--- test/eddtest.py
+++ test/eddtest.py
@@ -4,18 +4,14 @@ Created on 2017/02/12
 @author: rindybell
 '''
 import unittest
-import src.eddchars
+from src.eddchars import EddChars
 
 
 class Test(unittest.TestCase):
 
-    def __init__(self):
-        self.eddchars = src.eddchars.EddChars()
-
     def test_cost(self):
+        self.eddchars = EddChars()
         self.assertEqual(self.eddchars.distance("played", "play"), 2)
 
-
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
