6ed42ccdfb36e5f2e612df4a4136865c43c6a035
rindybell <rindybell@192.168.11.4>
2017-02-12 17:42:54 +0900

    modify testcode

--- test/eddtest.py
+++ test/eddtest.py
@@ -10,8 +10,12 @@ from src.eddchars import EddChars
 class Test(unittest.TestCase):
 
     def test_cost(self):
-        self.eddchars = EddChars()
-        self.assertEqual(self.eddchars.distance("played", "play"), 2)
+        eddchars = EddChars()
+        self.assertEqual(eddchars.distance("play", "played"), 2)
+        self.assertEqual(eddchars.distance("study", "studied"), 3)
+
+        eddchars = EddChars(sub_cost=2.0)
+        self.assertEqual(eddchars.distance("study", "studied"), 4)
 
 if __name__ == "__main__":
     unittest.main()
